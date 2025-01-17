d6f0ff3ca15ae878ee491b4e3cb011b1698c988a
ordokeld <109252899+ordokeld@users.noreply.github.com>
2025-01-17 14:01:20 +0100

    Add files via upload

---
+++ users_logs.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Окт 30 2023 г., 12:35
+-- Версия сервера: 10.4.28-MariaDB
+-- Версия PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `rfidattendance`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users_logs`
+--
+
+CREATE TABLE `users_logs` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `serialnumber` double NOT NULL,
+  `card_uid` varchar(30) NOT NULL,
+  `device_uid` varchar(20) NOT NULL,
+  `device_dep` varchar(20) NOT NULL,
+  `checkindate` date NOT NULL,
+  `timein` time NOT NULL,
+  `timeout` time NOT NULL,
+  `card_out` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Дамп данных таблицы `users_logs`
+--
+
+INSERT INTO `users_logs` (`id`, `username`, `serialnumber`, `card_uid`, `device_uid`, `device_dep`, `checkindate`, `timein`, `timeout`, `card_out`) VALUES
+(1, 'Matous', 2, '9817316581', 'b41d3c5f077d7704', 'sales', '2023-10-30', '14:18:10', '14:18:19', 1),
+(2, 'Hlavac', 3, '18413110818', 'b41d3c5f077d7704', 'sales', '2023-10-30', '14:18:15', '12:34:51', 1),
+(3, 'Eldiiar', 1, '42610814625382128', 'b41d3c5f077d7704', 'sales', '2023-10-30', '14:18:45', '14:18:48', 1),
+(4, 'Matous', 2, '9817316581', 'b41d3c5f077d7704', 'sales', '2023-10-30', '12:32:41', '12:34:27', 1),
+(5, 'Matous', 2, '9817316581', 'b41d3c5f077d7704', 'sales', '2023-10-30', '12:34:34', '00:00:00', 0),
+(6, 'Eldiiar', 1, '42610814625382128', 'b41d3c5f077d7704', 'sales', '2023-10-30', '12:34:45', '12:34:50', 1);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users_logs`
+--
+ALTER TABLE `users_logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users_logs`
+--
+ALTER TABLE `users_logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
